29fd65df783d3140e77c3282d5e9e3576b3e4c03
AlessandroEccel07 <ixm775@student.edu.ti.ch>
2026-05-20 17:10:36 +0200

    Creazione base Livello1
    Aggiunto:
    -background
    -Minigioco colori (con tutto quello che comprende)

--- DisinnescoBomba/core/src/main/java/io/github/some_example_name/Main.java
+++ DisinnescoBomba/core/src/main/java/io/github/some_example_name/Main.java
@@ -2,62 +2,89 @@ package io.github.some_example_name;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Container;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-import javax.swing.*;
+/**import javax.swing.*;
+import java.awt.*;*/
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-
-    private Texture texture;
-    private Stage stage;
+    private SpriteBatch batch;
+    private Texture background;
+    private ShapeRenderer shapeRenderer;
+    private float dotY= 670;
+    private float dotX= 610;
+    private float dotVelY=0;
+    private float dotVelX=0;
+    private Rectangle dotBounds;
+    private Rectangle colorRectanglebounds;
 
     @Override
     public void create() {
-        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
-        stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
-        texture = new Texture("libgdx.png");
-        TextButton button = new TextButton("Text Button", skin);
-        Container wrapper = new Container(button);
-        Image image = new Image(texture);
-        image.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                System.out.println("You clicked an image...");
-            }
-
-        });
-
-        stage.addActor(image);
+        shapeRenderer = new ShapeRenderer();
+        batch = new SpriteBatch();
+        background = new Texture("background.png");
+        dotBounds =new Rectangle(dotX, dotY,10,10);
+        colorRectanglebounds =new Rectangle(484, 558, 250,245);
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        stage.act(Gdx.graphics.getDeltaTime());
-        stage.draw();
-        wrapper.setTransform(true);
-        wrapper.setOrigin(wrapper.getPrefWidth() / 2, wrapper.getPrefHeight() / 2);
-        wrapper.setRotation(45);
-        wrapper.setScaleX(1.5f);
+        float dt = Gdx.graphics.getDeltaTime();
+        dotVelX = 0;
+        dotVelY = 0;
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            System.out.println("UP");
+            dotVelX = 100;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            System.out.println("DOWN");
+            dotVelX = -100;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            System.out.println("RIGHT");
+            dotVelY = 100;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            System.out.println("LEFT");
+            dotVelY = -100;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.Q)) {
+            System.out.println(dotX+","+ dotY);
+        }
+        dotY += dotVelX* dt;
+        dotX += dotVelY* dt;
+        if (dotBounds.overlaps(colorRectanglebounds)) {
+            System.out.println("Collision");
+        }
+        dotBounds.setPosition(dotX, dotY);
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        batch.draw(background, 0, 0);
+        batch.end();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.WHITE);
+        shapeRenderer.circle(dotX , dotY, 10);
+        shapeRenderer.end();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.WHITE);
+        shapeRenderer.rect(colorRectanglebounds.x , colorRectanglebounds.y, 250,245);
+        shapeRenderer.end();
     }
 
     @Override
     public void dispose() {
-        stage.dispose();
-        texture.dispose();
+        background.dispose();
+        shapeRenderer.dispose();
+        batch.dispose();
+
     }
 }
